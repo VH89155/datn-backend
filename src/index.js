005995cdb217c846848c748c68c9bd99b32593ae
hiep <hiepnmhh.hh@gmail.com>
2023-06-09 18:06:27 +0700

    a

--- src/index.js
+++ src/index.js
@@ -13,10 +13,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(cors({
-  origin: 'https://vh89155.github.io/dant-frontend',
-  
-}));
+app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "https://vh89155.github.io"); next(); });
 
 
 
