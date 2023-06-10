6388f3b2e848c1b487c6fd643497600d98a5a36e
hiep <hiepnmhh.hh@gmail.com>
2023-06-10 16:51:52 +0700

    Dsa

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ const app = express();
 // connect to DB
 db.connect();
 require("dotenv").config({path:'.env'})
-app.use(cors({
-  origin: 'https://vh89155.github.io/dant-frontend/', // Chỉ cho phép truy cập từ domain này
-  methods: ['GET', 'POST',''], // Chỉ cho phép sử dụng các phương thức GET và POST
-  credentials: true // Cho phép chuyển thông tin xác thực
-}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "https://vh89155.github.io/dant-frontend"); next(); });
+// app.use(cors({
+//   origin: 'https://vh89155.github.io/dant-frontend/', // Chỉ cho phép truy cập từ domain này
+//   methods: ['GET', 'POST',''], // Chỉ cho phép sử dụng các phương thức GET và POST
+//   credentials: true // Cho phép chuyển thông tin xác thực
+// }));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "https://vh89155.github.io/dant-frontend/"); next(); });
 
 
 
