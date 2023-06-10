4695b35dc10851e7d8030ad334cf75b306b4e3a9
hiep <hiepnmhh.hh@gmail.com>
2023-06-10 16:57:33 +0700

    Dsa

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "https://vh89155.github.io/dant-frontend/"); next(); });
+app.use(function(req, res, next) { res.setHeader("Access-Control-Allow-Origin", "https://vh89155.github.io"); next(); });
 
 
 
